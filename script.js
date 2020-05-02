64114c2a69e1a6fbca5c30d6b3e37c50ed3866a6
Axel Obscura <axosar@gmail.com>
2020-05-02 12:00:47 -0500

    Edit func

--- script.js
+++ script.js
@@ -107,7 +107,9 @@ var UIController = (function () {
         newQuestionText: document.getElementById("new-question-text"),
         adminOptions: document.querySelectorAll(".admin-option"),
         adminOptionsContainer: document.querySelector(".admin-options-container"),
-        insertedQuestionsWrapper: document.querySelector(".inserted-questions-wrapper")
+        insertedQuestionsWrapper: document.querySelector(".inserted-questions-wrapper"),
+        questionUpdateBtn: document.getElementById("question-update-btn"),
+        questionDeleteBtn: document.getElementById("question-delete-btn")
     };
 
     return {
@@ -149,6 +151,43 @@ var UIController = (function () {
 
             }
 
+        },
+
+        editQuestionList: function (event, storageQuestionList, addInpsDynFn) {
+
+            var getId, getStorageQuestionList, foundItem, placeInArr, optionHTML;
+
+            if ('question-'.indexOf(event.target.id)) {
+                getId = parseInt(event.target.id.split('-')[1]);
+                getStorageQuestionList = storageQuestionList.getQuestionCollection();
+                for (var i = 0; i < getStorageQuestionList.length; i++) {
+                    if (getStorageQuestionList[i].id === getId) {
+                        foundItem = getStorageQuestionList[i];
+                        placeInArr = i;
+                    }
+                }
+
+                domItems.newQuestionText.value = foundItem.questionText;
+
+                domItems.adminOptionsContainer.innerHTML = "";
+
+                optionHTML = "";
+
+                for (var x = 0; x < foundItem.options.length; x++) {
+                    optionHTML += `<div class="admin-option-wrapper">
+                                    <input type="radio" class="admin-option-${x}" name="answer" value="0">
+                                    <input type="text" class="admin-option admin-option-${x}" value="${foundItem.options[x]}">
+                                </div>`;
+                }
+
+                domItems.adminOptionsContainer.innerHTML = optionHTML;
+                domItems.questionUpdateBtn.style.visibility = "visible";
+                domItems.questionDeleteBtn.style.visibility = "visible";
+                domItems.questionInsertBtn.style.visibility = "hidden";
+                addInpsDynFn();
+            }
+
+
         }
     }
 
@@ -170,5 +209,9 @@ var controller = (function (quizCtrl, UiCtrl) {
             UIController.createQuestionList(quizController.getQuestionLocalStorage);
         }
     });
+
+    selectedDomitems.insertedQuestionsWrapper.addEventListener('click', function (e) {
+        UiCtrl.editQuestionList(e, quizCtrl.getQuestionLocalStorage, UIController.addInputsDynamically);
+    })
 
 })(quizController, UIController);
